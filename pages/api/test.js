4d162b2e478aa4bfcfcd8b72d9346a926c2ebd12
JoshiSankar-4021 <joshisankar5051@gmail.com>
2025-09-17 17:56:30 +0530

    prisma creation

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,17 +1,24 @@
 export default function handeler(req,res){
     if(req.method==="GET"){
-        res.status(200).json({message:`this is get api`})
+        if(req.query.action==="getuser"){
+            res.status(200).json({message:`this is get user get api`})
+        }
+        if(req.query.action==="getpost"){
+            res.status(200).json({message:`this is get post get api`})
+        }
     }
     if(req.method==="POST"){
-        res.status(200).json({message:`this is post api`})
-
+        if(req.query.action==="post"){
+        res.status(200).json({message:`this is get user get api`})
+        }
+         if(req.query.action==="post"){
+        res.status(200).json({message:`this is get user get api`})
+        }
     }
     if(req.method==="PUT"){
         res.status(200).json({message:`this is put api`})
-
     }
     if(req.method==="DELETE"){
         res.status(200).json({message:`this is delete api`})
-
     }
 }
