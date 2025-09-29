35ebc394751af0607013e38d59c342e01491f71f
JoshiSankar-4021 <joshisankar5051@gmail.com>
2025-09-29 17:26:51 +0530

    setup

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,10 @@
 import {pool} from '../../lib/db';
+import {cors} from '../../lib/cors';
 export default async function handeler(req,res){
+
+    const handled = cors(req, res);
+    if (handled) return;
+    
     if(req.method==="GET"){
         // if(req.query.action==="getuser"){
         //     res.status(200).json({message:`this is get user get api`})
@@ -11,7 +16,7 @@ export default async function handeler(req,res){
         if(req.query.action==="getusers"){
             const selectquery =`select * from "User"`;
             const result=await pool.query(selectquery);
-            res.status(200).json(result.rows)
+            res.status(200).json({users:result.rows})
         }
         if (req.query.action === "getuser") {
             const id = req.query.id;
@@ -30,11 +35,11 @@ export default async function handeler(req,res){
         }
 
         if(req.query.action==="adduser"){
-        const {id,name,email,password}=req.body;
+        const {name,email,password}=req.body;
         const insertquery = `
-        INSERT INTO "User"(id, name, email, password)
-        VALUES ($1, $2, $3, $4);`;
-        const values=[id,name,email,password];
+        INSERT INTO "User"(name, email, password)
+        VALUES ($1, $2, $3);`;
+        const values=[name,email,password];
         await pool.query(insertquery, values);
         res.status(200).json({message:"user registerred or added"});
         }
